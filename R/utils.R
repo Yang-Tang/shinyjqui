6f8ff563614901de2748f06e83949c1e5328f908
yang tang <tang_yang@outlook.com>
2017-03-05 21:48:40 +0800

    add jqui_icon()

--- R/utils.R
+++ R/utils.R
@@ -102,5 +102,31 @@ addInteractJS <- function(tag, func, options = NULL) {
 
 }
 
+#' Create a jQuery UI icon
+#'
+#' Create an jQuery UI pre-defined icon. For lists of available icons, see
+#' \url{http://api.jqueryui.com/theming/icons/}.
+#'
+#' @param name Class name of icon. The "ui-icon-" prefix can be omitted (i.e.
+#'   use "ui-icon-flag" or "flag" to display a flag icon)
+#'
+#' @return An icon element
+#' @export
+#'
+#' @examples
+#' jqui_icon('caret-1-n')
+#'
+#' # add an icon to an actionButton
+#' actionButton('button', 'Button', icon = jqui_icon('refresh'))
+#'
+#' # add an icon to a tabPanel
+#' tabPanel('Help', icon = jqui_icon('help'))
+jqui_icon <- function(name) {
+  if(!startsWith(name, 'ui-icon-')) {
+    name <- paste0('ui-icon-', name)
+  }
+  shiny::tags$i(class = paste0('ui-icon ', name))
+}
+
 
 
